0e434ca5ab27218af08bcde6fd11c1196f541262
kory <koryna654@gmail.com>
2025-05-07 17:43:07 -0500

    la foto si se puede ver, solo faltaba colocar en modelo muestra

--- resources/views/muestras/visitadoraMedica/index.blade.php
+++ resources/views/muestras/visitadoraMedica/index.blade.php
@@ -37,7 +37,7 @@
                                 @if($muestra->clasificacion && $muestra->clasificacion->unidadMedida)
                                     {{ $muestra->clasificacion->unidadMedida->nombre_unidad_de_medida }}
                                 @else
-                                    No asignada
+                                    No asignada unidadmedida
                                 @endif
                             </td>
                             <td>{{ ucfirst($muestra->tipo_muestra) ?? 'No asignado' }}</td>
@@ -73,7 +73,6 @@
             </table>
         </div>
     </div>
-
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css">
